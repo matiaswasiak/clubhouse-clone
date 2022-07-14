7d9de5998bb63c22ed080eada941e4acb1667183
Matias Wasiak <matiaswasiak@gmail.com>
2022-07-14 10:25:50 -0300

    clubhouse clone leaving here for now

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PhoneConfirmation from "./pages/PhoneConfirmation";
 import CodeConfirm from "./pages/CodeConfirm";
 import AllowNotification from "./pages/AllowNotification";
+import AppLayout from "./pages/Layouts/AppLayout";
+import Home from "./pages/Home";
 
 // <Playlayout>
 function App() {
@@ -19,6 +21,12 @@ function App() {
           <Route path="/allow_notification" element={<AllowNotification />} />
         </Routes>
       </PlanLayout>
+
+      <AppLayout>
+        <Routes>
+          <Route path="/home" element={() => <Home />}></Route>
+        </Routes>
+      </AppLayout>
     </BrowserRouter>
   );
 }
